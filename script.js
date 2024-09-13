7251679f09bf99581cad63ad8710aab130b13e48
abdul devoloper <shumaila.anees26@gmail.com>
2024-09-13 18:03:51 +0500

    task

---
+++ script.js
@@ -0,0 +1,7 @@
+<script>
+  document.getElementById('searchBar').addEventListener('input', function(e) {
+    let query = e.target.value.toLowerCase();
+    console.log('Searching for:', query);
+    // Implement search filter logic here
+  });
+</script>
